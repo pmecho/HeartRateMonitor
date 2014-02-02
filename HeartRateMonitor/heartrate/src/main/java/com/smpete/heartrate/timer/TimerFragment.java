31c1e98ff4bbc19f31ed40eade4736a72f8c9873
Peter Elliott <me@smpete.com>
2014-02-01 22:01:53 -0700

    Adding fork of better time pickers

--- HeartRateMonitor/heartrate/src/main/java/com/smpete/heartrate/timer/TimerFragment.java
+++ HeartRateMonitor/heartrate/src/main/java/com/smpete/heartrate/timer/TimerFragment.java
@@ -6,16 +6,43 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import com.doomonafireball.betterpickers.datepicker.DatePickerBuilder;
+import com.doomonafireball.betterpickers.hmspicker.HmsPickerBuilder;
+import com.doomonafireball.betterpickers.timepicker.TimePickerBuilder;
+import com.doomonafireball.betterpickers.timepicker.TimePickerDialogFragment;
 import com.smpete.heartrate.R;
 
-public class TimerFragment extends Fragment {
+public class TimerFragment extends Fragment implements TimePickerDialogFragment.TimePickerDialogHandler{
+    private static final int REFERENCE_WARM_UP = 200;
+
+
+    @InjectView(R.id.warm_up_time) Button mWarmUpTime;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hiit_setup, container, false);
-        container.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        ButterKnife.inject(this, v);
 
         return v;
+    }
+
+    @OnClick(R.id.warm_up_time)
+    public void onWarmUpTap() {
+        HmsPickerBuilder builder = new HmsPickerBuilder()
+                .setFragmentManager(getFragmentManager())
+                .setTargetFragment(this)
+                .setReference(REFERENCE_WARM_UP)
+                .setStyleResId(R.style.BetterPickersDialogFragment_Light);
+        builder.show();
+    }
+
+    @Override
+    public void onDialogTimeSet(int refernce, int minute, int second) {
+
     }
 }
