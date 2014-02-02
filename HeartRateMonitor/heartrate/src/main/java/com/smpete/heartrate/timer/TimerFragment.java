8999d22bc1c2186cf85294545857a08d69679a82
Peter Elliott <me@smpete.com>
2014-02-01 21:09:27 -0700

    Adding layout for HIIT setup

--- HeartRateMonitor/heartrate/src/main/java/com/smpete/heartrate/timer/TimerFragment.java
+++ HeartRateMonitor/heartrate/src/main/java/com/smpete/heartrate/timer/TimerFragment.java
@@ -1,6 +1,21 @@
 package com.smpete.heartrate.timer;
 
+import android.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import com.smpete.heartrate.R;
 
 public class TimerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hiit_setup, container, false);
+        container.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+        return v;
+    }
 }
